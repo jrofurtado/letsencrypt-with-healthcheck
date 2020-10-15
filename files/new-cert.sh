692205a377065e856cc02fc072158a1991fa8d88
jrofurtado <jrofurtado@gmail.com>
2020-10-15 16:00:16 +0000

    .

--- files/new-cert.sh
+++ files/new-cert.sh
@@ -1,4 +1,4 @@
-certbot certonly --standalone --preferred-challenges http --http-01-port 80 -n --agree-tos -m "jrofurtado@gmail.com" -d "$DOMAIN" \
+certbot certonly --standalone --preferred-challenges http --http-01-port 80 -n --agree-tos $PARAMS \
   && cp "/etc/letsencrypt/live/$DOMAIN/fullchain.pem" /volume/server.crt \
   && cp "/etc/letsencrypt/live/$DOMAIN/privkey.pem" /volume/server.key \
   && cat /volume/server.crt /volume/server.key > /volume/server.pem
